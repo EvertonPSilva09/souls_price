1993ea45c7169925627b1252a040197eb9fdfbaf
Everton Silva <everton.p.silva@outlook.com>
2025-05-10 14:54:06 -0300

    feat: add TypeOrmModule import for Soul entity in Souls module

--- src/souls/souls.module.ts
+++ src/souls/souls.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { SoulsService } from './souls.service';
 import { SoulsController } from './souls.controller';
+import { Soul } from './entities/soul.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Soul])],
   controllers: [SoulsController],
   providers: [SoulsService],
 })

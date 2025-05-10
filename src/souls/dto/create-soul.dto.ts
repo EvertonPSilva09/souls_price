2bc415cedd38ed6d7079ac813823f9cc337200af
Everton Silva <everton.p.silva@outlook.com>
2025-05-10 14:54:41 -0300

    feat: define CreateSoulDto with validation rules for soul properties

--- src/souls/dto/create-soul.dto.ts
+++ src/souls/dto/create-soul.dto.ts
@@ -1,1 +1,21 @@
-export class CreateSoulDto {}
+import { IsString, IsNumber } from 'class-validator';
+
+export class CreateSoulDto {
+  @IsString()
+  name: string;
+
+  @IsNumber()
+  price: number;
+
+  @IsNumber()
+  avg_price: number;
+
+  @IsString()
+  map: string;
+
+  @IsString()
+  monster: string;
+
+  @IsString()
+  rarity: string;
+}
